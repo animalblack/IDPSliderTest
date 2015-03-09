ec767569ef0d2a7f273ba8d4c2705aa9c30c5b52
Anton 'animal' Kovalyov <anton.kovalyov@idapgroup.com>
2015-03-10 00:05:29 +0200

    Initial Commit

---
+++ IDPSliderTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IDPSliderTest
+//
+//  Created by Anton Kovalyov on 3/10/15.
+//  Copyright (c) 2015 Anton Kovalyov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IDPSliderTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IDPSliderTest
+//
+//  Created by Anton Kovalyov on 3/10/15.
+//  Copyright (c) 2015 Anton Kovalyov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
